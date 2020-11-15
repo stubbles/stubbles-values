b82f06609bc24374277fc6154dc986181c1f89c8
Frank Kleine <mikey@bovigo.org>
2020-11-15 12:43:36 +0100

    fix expected message, different before PHP 7.4

--- src/test/php/PropertiesTest.php
+++ src/test/php/PropertiesTest.php
@@ -411,12 +411,13 @@ class PropertiesTest extends TestCase
      */
     public function invalidIniStringThrowsException(): void
     {
+        $end = version_compare(phpversion(), '7.4', '<=') ? '$end' : 'end';
         expect(function() {
                 Properties::fromString("[invalid{");
         })
         ->throws(\InvalidArgumentException::class)
         ->message(contains(
-            'Property string contains errors and can not be parsed: syntax error, unexpected end'
+            'Property string contains errors and can not be parsed: syntax error, unexpected ' . $end
         ));
     }
 
